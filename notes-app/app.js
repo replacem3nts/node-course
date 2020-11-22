1c448687c6977dfe408697a84e8639cae6d70339
replacem3nts <donny.landis@gmail.com>
2020-11-21 20:32:34 -0500

    completes section 3 incl global npm installs and other pckgs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const msg = getNotes();
-
 console.log(msg);
+
+const success = chalk.blue.bold.inverse('What?!');
+
+console.log(success);
