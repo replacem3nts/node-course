407b9e407d13284d07e45571d9021f9e75a191a4
replacem3nts <donny.landis@gmail.com>
2020-11-21 20:52:13 -0500

    adds in yargs and begins building the commands out

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,48 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs version
+yargs.version('1.1.0');
 
-const success = chalk.blue.bold.inverse('What?!');
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log('Adding a new note!');
+  },
+});
 
-console.log(success);
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove an existing note',
+  handler: () => {
+    console.log('Removing a note!');
+  },
+});
+
+// Create read command
+
+yargs.command({
+  command: 'read',
+  describe: 'Read an existing note',
+  handler: () => {
+    console.log('Reading a note!');
+  },
+});
+
+// Create list command
+
+yargs.command({
+  command: 'list',
+  describe: 'List the current notes',
+  handler: () => {
+    console.log('Listing all the notes!');
+  },
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
