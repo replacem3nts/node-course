f1631e27d35675172f0d3911797c6f3049ee10b5
replacem3nts <donny.landis@gmail.com>
2020-11-27 19:56:30 -0500

    adds geocoding and error handling to both http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,33 @@
 const postman = require('postman-request');
 
-const key = '...some key here...';
+const WSkey = '...weatherstack key';
 
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.765,-122.4233&units=f`;
+const WSurl = `http://api.weatherstack.com/current?access_key=${WSkey}&query=34.0544,-118.2439&units=f`;
 
-postman({ url: url, json: true }, (error, resp) => {
-  // console.log(resp.body.current);
-  const { temperature, feelslike, weather_descriptions } = resp.body.current;
-  console.log(
-    `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`
-  );
+postman({ url: WSurl, json: true }, (error, resp) => {
+  if (error) {
+    console.log('Unable to connect to weather service!');
+  } else if (resp.body.error) {
+    console.log(`${resp.body.error.info}`);
+  } else {
+    const { temperature, feelslike, weather_descriptions } = resp.body.current;
+    console.log(
+      `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`
+    );
+  }
+});
+
+const MBkey = '...mapbox key';
+
+const MBurl = `http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${MBkey}&limit=1`;
+
+https: postman({ url: MBurl, json: true }, (error, resp) => {
+  if (error) {
+    console.log('Unable to connect to geocoding service!');
+  } else if (resp.body.message) {
+    console.log(`${resp.body.message}`);
+  } else {
+    const { place_name, center } = resp.body.features[0];
+    console.log(`The lat/long of ${place_name} is ${center[0]}, ${center[1]}`);
+  }
 });
