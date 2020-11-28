0ee00c02c1a4d200e63c741e5ee0ff962a538ad9
replacem3nts <donny.landis@gmail.com>
2020-11-27 19:18:52 -0500

    completes weatherstack api requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,13 @@
 const postman = require('postman-request');
 
-const key = 'insert API key here...';
+const key = '...some key here...';
 
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=37.765,-122.4233&units=f`;
 
-postman({ url: url }, (error, resp) => {
-  console.log(resp);
+postman({ url: url, json: true }, (error, resp) => {
+  // console.log(resp.body.current);
+  const { temperature, feelslike, weather_descriptions } = resp.body.current;
+  console.log(
+    `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`
+  );
 });
