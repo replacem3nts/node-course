e74f281dd5654e52c5435d8450044d9b646b573f
replacem3nts <donny.landis@gmail.com>
2020-12-02 17:24:52 -0500

    finishes promises lesson

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -37,4 +37,17 @@
 //   console.log(sum); // Should print: 5
 // });
 
-const doWorkCallback = () => {};
+const doWorkCallback = callback => {
+  setTimeout(() => {
+    // callback('This is my error!', undefined);
+    callback(undefined, [1, 4, 7]);
+  }, 2000);
+};
+
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(result);
+});
