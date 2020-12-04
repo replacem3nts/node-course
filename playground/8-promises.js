bc74e18a7315e8873dfe4247de9ced3c3845223a
replacem3nts <donny.landis@gmail.com>
2020-12-04 17:25:16 -0500

    completes promise chaining practice example

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,18 +1,18 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve([7, 4, 1]);
-    reject('Things went wrong!');
-    resolve([2, 3, 2]);
-  }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve([7, 4, 1]);
+//     reject('Things went wrong!');
+//     resolve([2, 3, 2]);
+//   }, 2000);
+// });
 
-doWorkPromise
-  .then(result => {
-    console.log('Success!', result);
-  })
-  .catch(error => {
-    console.log('Error!', error);
-  });
+// doWorkPromise
+//   .then(result => {
+//     console.log('Success!', result);
+//   })
+//   .catch(error => {
+//     console.log('Error!', error);
+//   });
 
 //
 //                                fulfilled
@@ -21,3 +21,38 @@ doWorkPromise
 //                              \
 //                                rejected
 //
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+// add(1, 2)
+//   .then(sum => {
+//     console.log(sum);
+//     add(sum, 5)
+//       .then(sum2 => {
+//         console.log(sum2);
+//       })
+//       .catch(e => {
+//         console.log(e);
+//       });
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+add(1, 1)
+  .then(sum => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then(sum2 => {
+    console.log(sum2);
+  })
+  .catch(e => {
+    console.log(e);
+  });
