08da164b7e8b4c6922902438d14cddbe8ce7ed43
replacem3nts <donny.landis@gmail.com>
2020-12-05 11:59:40 -0500

    completes async-await lesson

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,3 +1,25 @@
-const doWork = () => {};
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject('Numbers must be non-negative');
+      }
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
-console.log(doWork());
+const doWork = async () => {
+  const sum = await add(5, 97);
+  const sum2 = await add(sum, 50);
+  const sum3 = await add(sum2, -3);
+  return sum3;
+};
+
+doWork()
+  .then(result => {
+    console.log('result', result);
+  })
+  .catch(e => {
+    console.log('e', e);
+  });
